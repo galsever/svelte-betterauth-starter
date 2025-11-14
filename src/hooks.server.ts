28aefd8da7b96d9618e80454532231884b3bfed1
srino <srinoytt@gmail.com>
2025-11-14 07:21:53 +0100

    chore: init (not done yet)

---
+++ src/hooks.server.ts
@@ -0,0 +1,6 @@
+import { auth } from "./auth.ts"; // path to your auth file
+import { svelteKitHandler } from "better-auth/svelte-kit";
+import { building } from '$app/environment'
+export async function handle({ event, resolve }) {
+    return svelteKitHandler({ event, resolve, auth, building });
+}
